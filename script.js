759902177c32bd0eacc0b3cf2e897ff7be62b675
David Munoz <97751073+David-Munoz-git@users.noreply.github.com>
2022-05-30 13:44:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var count = 9;
+var countLikes = document.querySelector('#likes');
+function add1(){
+    count++
+    countLikes.innerText = count + "like(s)" 
+    console.log(count)
+}
+// ---------------------------
+
+var countUp = 12
+var secondLikes = document.querySelector('#second');
+function addto(){
+    countUp++
+    secondLikes.innerText = countUp + "like(s)"
+}
+
+// -----------
+
+var counts = 9;
+var thirdLike = document.querySelector('#third');
+function addtoo(){
+    counts++
+    thirdLike.innerText = counts + "like(s)"
+}
